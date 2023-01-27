c528be6ff2f9ce0515c4ba646aff908ae124cb50
debmeetDev <debmeet.banerjee@gmail.com>
2023-01-27 17:23:42 +0530

    .

---
+++ 22_Threads/Demo.java
@@ -0,0 +1,92 @@
+// to create threads, we can either extend Thread Class or implement Runnable Interface
+
+class Hi extends Thread {
+    public void run() { // override the run method for the thread to work
+        for(int i=0;i<100;i++) {
+            System.out.println("Hi");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            
+        }
+    }
+}
+
+class Hello extends Thread {
+    public void run() {
+        for(int i=0;i<100;i++) {
+            System.out.println("Hello");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+////////////////////////////////////////////////////////////////////////////////////////////
+
+class Hey implements Runnable { // better to implement Runnable instead of Extending Thread
+    public void run() {
+        for(int i=0;i<100;i++) {
+            System.out.println("HeY...");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class Bye implements Runnable { 
+    public void run() {
+        for(int i=0;i<100;i++) {
+            System.out.println("Baaaye...");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        Hi hi = new Hi();
+        Hello hello = new Hello();
+
+        hi.run();// Untill all 10 hi are printed, Demo is on hold
+        hello.run();
+
+        // 1[least] to 10[max] priority, 5 is by default[normal]
+        System.out.println(hi.getPriority());
+        System.out.println(hello.getPriority());
+
+        System.out.println("............................................................");
+        System.out.println("............................................................");
+        System.out.println("............................................................");
+
+        // Start method is present in the Thread class. This wont work with Runnable
+        hi.start();     // Starts a thread and executes the run() method 
+        hello.start();  // Starts a thread and executes the run() method
+
+
+        // How to work with Runnable interface? 
+        // Create objects
+        Hey hey = new Hey();
+        Bye bye = new Bye();
+        // Create Thread using constructor and pass the Runnable object
+        Thread t1 = new Thread(hey);
+        Thread t2 = new Thread(bye);
+        // Start thread and execute the run() method of passed Runnable objects!
+        t1.start();
+        t2.start();
+
+        // IN this example all the 4 created threads would be running in parallel
+
+    }
+}
